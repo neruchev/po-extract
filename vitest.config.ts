cce92ac2cc31466dfefc61e7f7cfac4bb165cb92
Kirill Neruchev <kirill@neruchev.com>
2024-06-25 14:06:02 +0200

    Update configs

---
+++ vitest.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  resolve: {},
+  test: {
+    globals: true,
+    hookTimeout: 40000,
+    exclude: ['node_modules', 'build'],
+  },
+});
